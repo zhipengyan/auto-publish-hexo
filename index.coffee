eb2fcc78189f61ceee68f6195e1e7e19516f48a2
YanZhipeng <Sunny@YanZhipengdeMacBook-Pro.local>
2015-05-01 20:18:45 +0800

    test token

--- index.coffee
+++ index.coffee
@@ -3,37 +3,41 @@ shelljs=require "shelljs"
 
 http.createServer (request, response)->
   response.writeHead 200,{"Content-Type":"text/plain"}
-  currentDir = ''+shelljs.pwd()
-  hexoPostsDir = "#{currentDir}/../source/_posts"
-  hexoDir = "#{currentDir}/../"
-
-  #pull posts
-  shelljs.cd hexoPostsDir
-  pullCmd = shelljs.exec "ls & git pull origin master "  
-  result = false
-
-  if pullCmd.code is 0 
-    #pull successed!
-    console.log "pull successed!"
-    
-    #open node, if you are not using nvm please skip this step
-    if not (shelljs.which 'node')
-      nvmCmd = shelljs.exec "nvm use 0.12"
-
-    shelljs.cd hexoDir
-    hexoCmd = shelljs.exec "hexo clean & hexo generate"
-    if hexoCmd.code isnt 0
-      console.log "hexo generate failed!"
-      result = false
-    else
-      console.log "hexo generate successed!"
-    result = true
+  if request.body.SECRET_TOKEN is 'yan881224'
+    response.write 'yan881224'
+    response.end()
   else
-    console.log "pull posts failed"
+    currentDir = ''+shelljs.pwd()
+    hexoPostsDir = "#{currentDir}/../source/_posts"
+    hexoDir = "#{currentDir}/../"
+
+    #pull posts
+    shelljs.cd hexoPostsDir
+    pullCmd = shelljs.exec "ls & git pull origin master "  
     result = false
 
-  shelljs.cd currentDir
-  response.write ''+result
-  response.end()
+    if pullCmd.code is 0 
+      #pull successed!
+      console.log "pull successed!"
+      
+      #open node, if you are not using nvm please skip this step
+      if not (shelljs.which 'node')
+        nvmCmd = shelljs.exec "nvm use 0.12"
+
+      shelljs.cd hexoDir
+      hexoCmd = shelljs.exec "hexo clean & hexo generate"
+      if hexoCmd.code isnt 0
+        console.log "hexo generate failed!"
+        result = false
+      else
+        console.log "hexo generate successed!"
+      result = true
+    else
+      console.log "pull posts failed"
+      result = false
+
+    shelljs.cd currentDir
+    response.write ''+result
+    response.end()
   return
 .listen 8888
--- index.js
+++ index.js
@@ -11,33 +11,38 @@
     response.writeHead(200, {
       "Content-Type": "text/plain"
     });
-    currentDir = '' + shelljs.pwd();
-    hexoPostsDir = "" + currentDir + "/../source/_posts";
-    hexoDir = "" + currentDir + "/../";
-    shelljs.cd(hexoPostsDir);
-    pullCmd = shelljs.exec("ls & git pull origin master ");
-    result = false;
-    if (pullCmd.code === 0) {
-      console.log("pull successed!");
-      if (!(shelljs.which('node'))) {
-        nvmCmd = shelljs.exec("nvm use 0.12");
-      }
-      shelljs.cd(hexoDir);
-      hexoCmd = shelljs.exec("hexo clean & hexo generate");
-      if (hexoCmd.code !== 0) {
-        console.log("hexo generate failed!");
-        result = false;
-      } else {
-        console.log("hexo generate successed!");
-      }
-      result = true;
+    if (request.body.SECRET_TOKEN === 'yan881224') {
+      response.write('yan881224');
+      response.end();
     } else {
-      console.log("pull posts failed");
+      currentDir = '' + shelljs.pwd();
+      hexoPostsDir = "" + currentDir + "/../source/_posts";
+      hexoDir = "" + currentDir + "/../";
+      shelljs.cd(hexoPostsDir);
+      pullCmd = shelljs.exec("ls & git pull origin master ");
       result = false;
+      if (pullCmd.code === 0) {
+        console.log("pull successed!");
+        if (!(shelljs.which('node'))) {
+          nvmCmd = shelljs.exec("nvm use 0.12");
+        }
+        shelljs.cd(hexoDir);
+        hexoCmd = shelljs.exec("hexo clean & hexo generate");
+        if (hexoCmd.code !== 0) {
+          console.log("hexo generate failed!");
+          result = false;
+        } else {
+          console.log("hexo generate successed!");
+        }
+        result = true;
+      } else {
+        console.log("pull posts failed");
+        result = false;
+      }
+      shelljs.cd(currentDir);
+      response.write('' + result);
+      response.end();
     }
-    shelljs.cd(currentDir);
-    response.write('' + result);
-    response.end();
   }).listen(8888);
 
 }).call(this);
